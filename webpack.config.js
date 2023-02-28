dd2743134a0d9a147085bb0b82e33e9168ce36e6
Bryce Anderson <abryce.anderson@gmail.com>
2023-02-27 22:22:36 -0500

    repo set up with react, express, webpack, babel

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// webpack.config.js
+const path = require('path')
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './dist')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+}
